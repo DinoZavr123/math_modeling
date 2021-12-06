effa41e14b8f1a9d0a357e68541f797152337670
DinoZavr123 <91481903+DinoZavr123@users.noreply.github.com>
2021-12-06 15:32:21 +0000

    pp

--- p5-t4.py
+++ p5-t4.py
@@ -1,12 +1,21 @@
 import matplotlib.pyplot as plt
 import numpy as np
-import math 
+import math
+from math  import sqrt
+n = int(input()) 
 b=0.02
 N=1000
 phi0=0.01
 phi1=8*math.pi
 phi= np.linspace(phi0,phi1,N)
-r=np.exp(b*phi)
+if(n==1):
+  r=np.exp(b*phi)
+if(n==2):
+  r=b*phi
+if(n==3):
+  r=b/(np.sqrt(phi))
+if(n==4):
+  r=np.sin(b*phi)
 x=[]
 y=[]
 for i in range (N):
