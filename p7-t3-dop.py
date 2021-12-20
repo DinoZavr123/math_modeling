35b10d31486e051a6cf7525200c4af9175968962
DinoZavr123 <91481903+DinoZavr123@users.noreply.github.com>
2021-12-20 16:41:37 +0000

    t346y5y546

--- p7-t3-dop.py
+++ p7-t3-dop.py
@@ -6,21 +6,29 @@ fig, ax = plt.subplots()
 line, = plt.plot([], [], '--', color='red', label='line')
 edge = 40
 plt.axis('equal')
- 
+
 ax.set_xlim(-edge, edge)
 ax.set_ylim(-edge, edge)
  
 x, y = [], []
 
-def star():
-    t=np.arange(-10,10,0.01)
-    x = 12*np.cos(t)+ 8*np.cos(1.5*t)
-    y = 12*np.sin(t)-8*np.sin(1.5*t)
-    star, = plt.plot(x,y, '--', color='red', label='line')
-    t = np.pi /3
-    X = x*np.cos(t) - y*np.sin(t)
-    Y = y*np.cos(t) + x*np.sin(t)
-    star, = plt.plot(X,Y, '--', color='purple', label='line')
+t=np.arange(0,4*np.pi,0.1)
+x = 12*np.cos(t)+ 8*np.cos(1.5*t)
+y = 12*np.sin(t)-8*np.sin(1.5*t)
+t = np.pi 
+X = x*np.cos(t) - y*np.sin(t)
+Y = y*np.cos(t) + x*np.sin(t)
 
-star()
-plt.show()
+def rotate(x,y,angle):
+    X = x*np.cos(angle) - y*np.sin(angle)
+    Y = y*np.cos(angle) + x*np.sin(angle)
+    return X,Y
+
+def animate(a):
+    line.set_data(rotate(x,y,a))
+
+ani = animation.FuncAnimation(fig,
+                        animate,
+                        frames=np.arange(0,2*np.pi,np.pi/180),
+                        interval=5)
+ani.save('my_anim.gif')
